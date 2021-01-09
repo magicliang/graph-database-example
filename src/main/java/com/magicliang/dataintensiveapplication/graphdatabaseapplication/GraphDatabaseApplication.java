ee769aa725503cc7c2a2099aa7828d50eda03906
magicliang <liangchuan@meituan.com>
2021-01-09 17:32:02 +0800

    init project

---
+++ src/main/java/com/magicliang/dataintensiveapplication/graphdatabaseapplication/GraphDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.magicliang.dataintensiveapplication.graphdatabaseapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magicliang/dataintensiveapplication/graphdatabaseapplication/GraphDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magicliang.dataintensiveapplication.graphdatabaseapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

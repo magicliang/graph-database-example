31dbbc2ac56562bba45a74ddc66d0e964931d775
magicliang <liangchuan@meituan.com>
2021-01-09 21:33:21 +0800

    初始化数据源

--- biz-service-impl/src/main/java/com/magicliang/dataintensiveapplication/graphdatabaseapplication/GraphDatabaseApplication.java
+++ biz-service-impl/src/main/java/com/magicliang/dataintensiveapplication/graphdatabaseapplication/GraphDatabaseApplication.java
@@ -2,8 +2,9 @@ package com.magicliang.dataintensiveapplication.graphdatabaseapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class GraphDatabaseApplication {
 
 	public static void main(String[] args) {
